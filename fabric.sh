2b69395a31d88b11409a7184c04e24c8ec53bf23
myeongkil <clevermk7211@gmail.com>
2020-11-15 21:13:29 +0900

    refactoring command params

--- fabric.sh
+++ fabric.sh
@@ -1,14 +1,15 @@
+#!/bin/bash
 rootdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 bdir=$rootdir/build
+adir=$rootdir/artifacts
 
 function docker_env {
     o=$1
-    O=$2
     dop=build/crypto-config/ordererOrganizations/smartm2m.co.kr/tlsca
     dpp=build/crypto-config/peerOrganizations/$o.smartm2m.co.kr/peers/peer0.$o.smartm2m.co.kr
     dap=build/crypto-config/peerOrganizations/$o.smartm2m.co.kr/users/Admin@$o.smartm2m.co.kr
     header="docker exec -it \
-        -e CORE_PEER_LOCALMSPID=${O}MSP \
+        -e CORE_PEER_LOCALMSPID=${o}MSP \
         -e CORE_PEER_MSPCONFIGPATH=$dap/msp \
         -e CORE_PEER_TLS_CERT_FILE=$dpp/tls/server.crt \
         -e CORE_PEER_TLS_KEY_FILE=$dpp/tls/server.key \
@@ -26,39 +27,53 @@ function docker_env {
 }
 
 function cryptogen {
-    docker run -i -t --rm -v $(pwd):/crypto --workdir /crypto \
+    docker run -i -t --rm -v $(pwd):/usr/app --workdir /usr/app \
         hyperledger/fabric-tools:1.4.8 \
-        cryptogen generate --config=crypto-config.yaml
+        cryptogen generate --config=artifacts/crypto-config.yaml
+    mv $rootdir/crypto-config $bdir/crypto-config
 }
 
 function configtxgen {
-    docker run -i -t --rm -v $(pwd):/usr/app --workdir /usr/app \
+    cp $adir/configtx.yaml $bdir/configtx.yaml
+    docker run -i -t --rm -v $bdir:/usr/app --workdir /usr/app \
         -e FABRIC_CFG_PATH=/usr/app \
         hyperledger/fabric-tools:1.4.8 \
         configtxgen -outputBlock mc.block -profile MainChannel -channelID mc
 
-    docker run -i -t --rm -v $(pwd):/usr/app --workdir /usr/app \
+    docker run -i -t --rm -v $bdir:/usr/app --workdir /usr/app \
         -e FABRIC_CFG_PATH=/usr/app \
         hyperledger/fabric-tools:1.4.8 \
         configtxgen -outputCreateChannelTx tca.tx -profile TestChannelA -channelID tca
 
-    docker run -i -t --rm -v $(pwd):/usr/app --workdir /usr/app \
+    docker run -i -t --rm -v $bdir:/usr/app --workdir /usr/app \
         -e FABRIC_CFG_PATH=/usr/app \
         hyperledger/fabric-tools:1.4.8 \
         configtxgen -outputCreateChannelTx tcb.tx -profile TestChannelB -channelID tcb
 
-    docker run -i -t --rm -v $(pwd):/usr/app --workdir /usr/app \
+    docker run -i -t --rm -v $bdir:/usr/app --workdir /usr/app \
         -e FABRIC_CFG_PATH=/usr/app \
         hyperledger/fabric-tools:1.4.8 \
         configtxgen -outputCreateChannelTx tcc.tx -profile TestChannelC -channelID tcc
 
-    mv $rootdir/mc.block $bdir/block/mc.block
-    mv $rootdir/*.tx $bdir/tx/
+    rm $bdir/configtx.yaml
+    mv $bdir/mc.block $bdir/block/mc.block > /dev/null 2>&1
+    mv $bdir/*.tx $bdir/tx/ > /dev/null 2>&1
+}
+
+function envgen {
+    orgs=(pusan ulsan seoul)
+    touch $rootdir/.env
+    for org in ${orgs[@]}
+    do
+        private_key=$(cd $bdir/crypto-config/peerOrganizations/${org}.smartm2m.co.kr/ca && ls *_sk)
+        echo "ca_${org}_private_key=$private_key" >> .env
+    done
 }
 
 function generate {
     cryptogen
     configtxgen
+    envgen
 }
 
 function build {
@@ -71,6 +86,7 @@ function build {
 function clean {
     down
     docker network rm blockchain > /dev/null 2>&1
+    rm .env
     rm -rf $bdir
 }
 
@@ -84,101 +100,110 @@ function up {
 }
 
 function channel_create {
-    shift
     org=$1
-    docker_env $org $2
-    channel=$3
+    docker_env $org
+    channel=$2
 
     $header peer0.$org.smartm2m.co.kr peer channel create -c $channel -f build/tx/$channel.tx -o orderer0.smartm2m.co.kr:7050 $tail
     mv $channel.block $bdir/block/
 }
 
 function channel_join {
-    shift
     org=$1
-    docker_env $org $2
-    channel=$3
+    docker_env $org
+    channel=$2
 
     $header peer0.$org.smartm2m.co.kr peer channel join -b build/block/$channel.block
 }
 
 function chaincode_install {
-    shift
     org=$1
-    docker_env $org $2
+    docker_env $org
 
     $header cli.smartm2m.co.kr peer chaincode install -n mycc -v 1.0 -p github.com/myeongkil/simple-fabric-network/chaincode/sacc
 }
 
 function chaincode_instantiate {
-    shift
     org=$1
-    docker_env $org $2
-    channel=$3
+    docker_env $org
+    channel=$2
     cc_init_input='{"Args":["kmk","500"]}'
     $header cli.smartm2m.co.kr peer chaincode instantiate -n mycc -v 1.0 -C $channel -c $cc_init_input -o orderer0.smartm2m.co.kr:7050 $tail
 }
 
 function chaincode_invoke {
-    shift
     org=$1
-    docker_env $org $2
-    channel=$3
+    docker_env $org
+    channel=$2
     cc_input='{"Args":["set","zz","500"]}'
     $header cli.smartm2m.co.kr peer chaincode invoke -n mycc -C $channel -c $cc_input -o orderer0.smartm2m.co.kr:7050 $tail
 }
 
 function chaincode_query {
-    shift
     org=$1
-    docker_env $org $2
-    channel=$3
+    docker_env $org
+    channel=$2
     cc_input='{"Args":["get","zz"]}'
     $header cli.smartm2m.co.kr peer chaincode query -n mycc -C $channel -c $cc_input
 }
 
 function channel {
+    cmd=$1
     shift
-    channel_$1 $@
+    channel_$cmd $@
 }
 
 function chaincode {
+    cmd=$1
     shift
-    chaincode_$1 $@
+    chaincode_$cmd $@
 }
 
 function all {
-    # clean
-    # build
-    # up
-    # down
-    # up
-    # channel create create pusan Pusan tca
-    # channel create create pusan Pusan tcb
-    # channel create create ulsan Ulsan tcc
-    # channel join join pusan Pusan tca
-    # channel join join pusan Pusan tcb
-    # channel join join ulsan Ulsan tca
-    # channel join join ulsan Ulsan tcc
-    # channel join join seoul Seoul tcb
-    # channel join join seoul Seoul tcc
+    # Clean all
+    clean
 
-    # chaincode install install pusan Pusan
-    # chaincode install install ulsan Ulsan
-    # chaincode install install seoul Seoul
-    chaincode instantiate instantiate pusan Pusan tca
-    chaincode instantiate instantiate pusan Pusan tcb
-    chaincode instantiate instantiate ulsan Ulsan tcc
+    # Build Artifacts (crypto-config, genesis.block, channel.tx)
+    build
+
+    # Network up (orderer, ca, peer, statedb, cli)
+    up
+
+    # Channel create (tca, tcb, tcc)
+    channel create pusan tca
+    channel create pusan tcb
+    channel create ulsan tcc
+
+    # Channel join (pusan, ulsan, seoul)
+    channel join pusan tca
+    channel join pusan tcb
+
+    channel join ulsan tca
+    channel join ulsan tcc
+
+    channel join seoul tcb
+    channel join seoul tcc
+
+    # Chaincode install (sacc-mycc:1.0 -> peer0.pusan, peer0.ulsan, peer0.seoul)
+    chaincode install pusan
+    chaincode install ulsan
+    chaincode install seoul
+
+    # Chaincode instantiate (mycc:1.0 -> tca, tcb, tcc)
+    chaincode instantiate pusan tca
+    chaincode instantiate pusan tcb
+    chaincode instantiate ulsan tcc
 }
 
 function main {
     if [[ -n $1 ]]
     then
-        $1 $@
+        cmd=$1
+        shift
+        $cmd $@
     else
         echo "is not supported Commands"
     fi
 }
 
 main $@
-# cryptogen
